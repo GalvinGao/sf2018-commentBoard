459ad7211f4ce4778b53aa294c1080ecdd518554
Galvin Gao <2403901511@qq.com>
2018-02-14 16:09:50 +0800

    >

--- server/config.js
+++ server/config.js
@@ -2,8 +2,8 @@ const config = {
 	adminUrl: "/admin/stats",
 	evalUrl: "/admin/api/eval",
 	adminToken: "PFaHq1hCgq4jovF3zEmalezz",
-	keypath: "/etc/letsencrypt/live/sf2018.dev.iblueg.cn/privkey.pem", // HTTPS Server Private Key
-	certpath: "/etc/letsencrypt/live/sf2018.dev.iblueg.cn/fullchain.pem", // HTTPS Server Certificate		
+	keypath: "/etc/letsencrypt/live/dev.khs.science/privkey.pem", // HTTPS Server Private Key
+	certpath: "/etc/letsencrypt/live/dev.khs.science/fullchain.pem", // HTTPS Server Certificate		
 	// HTTP jumps to HTTPS - Hostname Config
 	// PLEASE MAKE SURE THIS IS CORRECTLY CONFIGURED!
 	// NO NEED FOR "/" OR "http://"!
